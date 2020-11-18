de300c71e639f1f8528e3d6884f9384ad2a4a4f5
Andrei Avram <avramandreitiberiu@gmail.com>
2020-11-18 19:41:18 +0000

    -layout for login screen done

--- app/src/main/java/com/andrei/compassapp/fragments/LoginFragment.kt
+++ app/src/main/java/com/andrei/compassapp/fragments/LoginFragment.kt
@@ -1,19 +1,24 @@
 package com.andrei.compassapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.andrei.compassapp.R
-import com.andrei.compassapp.customViews.GoogleSignInButton
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.andrei.compassapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+       binding.welcomeLoginButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToLoginUsernameFragment()
+            findNavController().navigate(action)
+       }
+        return binding.root
     }
 }
---
+++ app/src/main/java/com/andrei/compassapp/fragments/LoginUsernameFragment.kt
@@ -0,0 +1,19 @@
+package com.andrei.compassapp.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andrei.compassapp.R
+
+class LoginUsernameFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_username_login, container, false)
+    }
+}
