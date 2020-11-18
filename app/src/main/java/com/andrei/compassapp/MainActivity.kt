9a9a2f080a03619613b778d90bf59e56717ab038
Andrei Avram <avramandreitiberiu@gmail.com>
2020-11-18 01:46:47 +0000

    Initial commit

---
+++ app/src/main/java/com/andrei/compassapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.andrei.compassapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.andrei.compassapp.fragments.LoginFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.layout_main_activity,LoginFragment()).commit()
+    }
+}
