f895542b47861a1cfe948e8799457cf75d162693
Andrei Avram <avramandreitiberiu@gmail.com>
2020-11-20 00:05:07 +0000

    -added Amazon auth

--- app/src/main/java/com/andrei/compassapp/CompassApplication.kt
+++ app/src/main/java/com/andrei/compassapp/CompassApplication.kt
@@ -3,11 +3,15 @@ package com.andrei.compassapp
 import android.app.Application
 import android.util.Log
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
 
 class CompassApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
+        Amplify.addPlugin(AWSCognitoAuthPlugin())
+
         try {
             Amplify.configure(applicationContext)
             Log.i("MyAmplifyApp", "Initialized Amplify")
